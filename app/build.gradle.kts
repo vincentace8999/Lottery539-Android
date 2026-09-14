0d5380b59172d5ec9148a4ab5586213cebb73bb8
vincentace8999 <vincentace8@gmail.com>
2026-09-14 20:13:28 +0800

    Fix Java and Kotlin JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,11 @@ android {
         versionName = "1.0.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildFeatures { compose = true }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
