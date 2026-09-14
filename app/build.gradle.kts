f192deee52c82166a99f1d93847532d577f16229
vincentace8999 <vincentace8@gmail.com>
2026-09-14 21:01:41 +0800

    Release 1.1.0 with tail-number analysis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vincent.lottery539"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     compileOptions {
